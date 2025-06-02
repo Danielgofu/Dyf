7bdd997d500a9d40172db773c8692050ca2f6644
Danielgofu <Danielgofu8@gmail.com>
2025-06-02 14:11:04 +0200

    Añadir paginas nuevas

---
+++ sw.js
@@ -0,0 +1,13 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('dyf-static').then(cache => cache.addAll([
+      '/offline.html'
+    ]))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match('/offline.html'))
+  );
+});
